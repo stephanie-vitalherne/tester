2a303b6191b15de6efcac6a4a6a6a3aef321bb4f
Stephanie Vital-Herne <vitalherne@gmail.com>
2020-01-11 00:57:31 -0500

    initial commit

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SigninScreen = () => {
+  return <Text>BOOBS</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SigninScreen;
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SignupScreen = () => {
+  return <Text>BOOBS</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
