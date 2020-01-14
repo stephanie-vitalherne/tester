191322fb0b8d2642cc5b9d9a5715a4d4ca36b71d
Stephanie Vital-Herne <vitalherne@gmail.com>
2020-01-13 21:28:07 -0500

    setting up the authorization so that the user can sign up and sign in and sign out

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
 
+// Components
 import AccountScreen from './src/screens/AccountScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
+import { Provider as AuthProvider } from './src/context/AuthContext';
 
 // Switch Navigator so the app can abruptly switch from the login/signup screen to the dashboard
 const switchNavigator = createSwitchNavigator({
@@ -26,4 +28,12 @@ const switchNavigator = createSwitchNavigator({
   })
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
