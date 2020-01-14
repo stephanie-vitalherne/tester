e2ec2185256f37f0d748c656cc12a6359c22f3d5
Stephanie Vital-Herne <vitalherne@gmail.com>
2020-01-13 23:05:57 -0500

    making boilerplate for the auth side of the app so that the api made before can finally talk with the app and the database can be attached and used properly

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = dispatch => {
+  return ({ email, password }) => {
+    // make api request to sign up with email and password
+    // if signed up, modify the state and say that user is authenticated
+    // if sign up fails, show error message
+  };
+};
+
+const login = dispatch => {
+  return ({ email, password }) => {
+    // try to login
+    // if logged in, modify the state
+    // if login fails, show error message
+  };
+};
+
+const signout = dispatch => {
+  return () => {
+    // sign out
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signup, login, signout },
   { isSignedIn: false }
 );
