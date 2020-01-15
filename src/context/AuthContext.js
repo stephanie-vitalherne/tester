95bea429e1a8dbfec687a9e8b6df438647605147
Stephanie Vital-Herne <vitalherne@gmail.com>
2020-01-14 22:50:03 -0500

    quick refactoring coming up

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
 
@@ -5,6 +6,8 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { errorMessage: '', token: action.payload };
     default:
       return state;
   }
@@ -15,7 +18,8 @@ const signup = dispatch => {
     // make api request to sign up with email and password
     try {
       const response = await trackerApi.post('/signup', { email, password });
-      console.log(response.data);
+      await AsyncStorage.setItem('token', response.data.token);
+      dispatch({ type: 'signup', payload: response.data.token });
     } catch (err) {
       dispatch({
         type: 'add_error',
@@ -44,5 +48,5 @@ const signout = dispatch => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signup, login, signout },
-  { isSignedIn: false, errorMessage: '' }
+  { token: null, errorMessage: '' }
 );
