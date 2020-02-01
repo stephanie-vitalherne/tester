97071669d529fb43116ac40c1625c008b5dcacc0
Stephanie Vital-Herne <vitalherne@gmail.com>
2020-02-01 00:26:05 -0500

    maps works now, added some polyline for showing a line on the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
-  return <MapView style={styles.map} />;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.33233,
+        longitude: -122.03121,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}>
+      <Polyline />
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
